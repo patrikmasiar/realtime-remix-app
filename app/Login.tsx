d19074d0a1b1c542b30041c9a5044e7a9a4680bf
Patrik Mäsiar <masiar.patrik@gmail.com>
2023-11-23 20:34:33 +0100

    Rename login title

--- app/Login.tsx
+++ app/Login.tsx
@@ -8,7 +8,7 @@ export default function Example({onSubmit}: {onSubmit: (data: {name: string; roo
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Enter the game
+            Enter the chat
           </h2>
         </div>
 
